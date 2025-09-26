2cb82368804e4737247b42ee756b9a4fa88b9387
Amir <vereshchahin01@gmail.com>
2025-09-26 16:56:42 +0300

    hw4

---
+++ jsBasic/task2.js
@@ -0,0 +1,8 @@
+const name1 = "Антон"
+const name2 = "Марія"
+
+const helloDear = "Привіт, " + name1 + " і " + name2 + "!"
+console.log(helloDear);
+
+const helloDear2 = `Привіт, ${name1} і ${name2}!`
+console.log(helloDear2);
