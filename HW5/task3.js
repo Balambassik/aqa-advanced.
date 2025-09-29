cc083c5eb4c5bdc6d36d8962788bdf08a793f12d
Amir <vereshchahin01@gmail.com>
2025-09-29 17:29:22 +0300

    HW5

---
+++ HW5/task3.js
@@ -0,0 +1,15 @@
+const number = 6
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`)
+}
+
+console.log("-----------------")
+
+const number2 = 4
+let count = 1
+
+while(count <= 10){
+    console.log(`${number2} * ${count} = ${count * number2}`)
+    count += 1
+}
